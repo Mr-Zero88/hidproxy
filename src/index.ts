ff40d87fdbed75a65b3d91b5baaefb00aae0cf8f
Mats Huesmann <huesmann.mats@gmail.com>
2025-12-01 10:14:42 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import HID = require("node-hid");
+import * as WebSocket from 'ws';
+
+process.title = "@kinder-der-toten-stadt/light-control@1.0.0";
+
+let devices = HID.devices().filter((dev) => dev.productId == 4613 && dev.vendorId == 10462);
+let sl: Array<WebSocket> = [];
+let websocketserver = new WebSocket.Server({port: 7000});
+websocketserver.on('connection', s => sl.push(s))
+let sendRaw = (data: Buffer) => {
+  sl.forEach(_ => _.send(data));
+};
+console.log(devices);
+let device = devices[3];
+if (device == null) process.abort();
+let controler = new HID.HID(device.path ?? "");
+controler.on('data', (data: Buffer) => {
+  sendRaw(data);
+});
